166f8fb9375ccc857d61eb46f56dad629742729c
tiana <tianasjohnson98@gmail.com>
2021-10-24 17:24:27 -0400

    script setup

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const startButton = document.getElementsByClassName('start-btn')
+
+
+startButton.addEventlistener('click', startQuiz)
+
+function startQuiz() {
+
+
+
+}
+
+function nextPage() {
+
+
+
+}
+
+function endQuiz() {
+
+
+
+}
+
