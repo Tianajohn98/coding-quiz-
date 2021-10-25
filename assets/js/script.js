e0f5731bec952df97dfdf44b8b421c0284cb197f
tiana <tianasjohnson98@gmail.com>
2021-10-25 17:43:06 -0400

    quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 const startButton = document.getElementById('start-btn')
 const quizContainerElement = document.getElementById('quiz-container')
-
 const questionElement = document.getElementById('quiz-questions')
 const answerButtonElement = document.getElementById('answer-buttons')
-const questionAnswers = document.getElementsByClassName('btn')
+
 let randomQues, currentQues 
 
 
+
 startButton.addEventListener('click', startQuiz)
+
+
+
+
 
 function startQuiz() {
 startButton.classList.add('hide')
@@ -32,37 +36,46 @@ function showQuestion(questions){
 questionElement.innerText = questions.question
 questions.answers.forEach(answers => {
     const button = document.createElement('button')
-    button.innerText =  answers.text
+    button.innerText = answers.text
     button.classList.add('btn')
-    if (answers.correct) {
+    if (answers.correct ) {
         button.dataset.correct = answers.correct
     }
-    button.addEventListener('click',(selectAnswer))
+    button.addEventListener('click', selectAnswer)
     answerButtonElement.appendChild(button)
-}); 
+})
+
+   
 
 }
 
-function reset() {
+function selectAnswer(e) {
+const selectedButton = e.target
+const correct = selectedButton.dataset.correct
+if (selectedButton.correct = true) {
+    nextPage()
+}
+
     
-    while (answerButtonElement.firstChild ) {
+
+}
+
+
+
+  function reset() {
+
+   while (answerButtonElement.firstChild ) {
     answerButtonElement.removeChild
-    (answerButtonElement.firstChild)
+   (answerButtonElement.firstChild)
     }
 }
 
 
-
-function selectAnswer() {
-
-
- 
-}
 
 const questions = [
 
     {
-       question: 'In the Doms event object, what does its target property refer to ?',
+       question: 'In the Doms event object, what does its target property refer to ?',''
        answers:[
         { text: 'It refers to the HTML element that was interacted with to dispatch the event', correct: true},
         { text: 'It refers to the HTML element we want to affect as a result of our dispatched event', correct:false},
